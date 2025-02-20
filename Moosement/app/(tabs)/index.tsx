a0509e5ea59aa2d81d3a64051d5b85bb2144ce83
Kayvan <kayvan.mosharaf@gmail.com>
2025-02-20 18:45:46 -0500

    not finished but working on sign up page

--- Moosement/app/(tabs)/index.tsx
+++ Moosement/app/(tabs)/index.tsx
@@ -1,13 +1,16 @@
-import { StyleSheet } from 'react-native';
+import { Button, StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import { router } from 'expo-router';
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Tab Hel</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Button title="Go Back to Sign Up" onPress={() => router.replace('/signup')} />
+      
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
   );
