50350c1ca49829381b802d0fa7b92516e3b45f7f
riyajroy23 <riyajroy23@gmail.com>
2025-02-28 17:40:45 -0500

    added comments for the major code segments

--- login.py
+++ login.py
@@ -13,18 +13,21 @@ users_collection = db["user_data"]
 def login():
     data = request.get_json()
 
+    # check that the user entered in an email and a password
     required_fields = ["email", "password"]
     for field in required_fields:
         if field not in data:
             return jsonify({"error": f"Missing field: {field}"}), 400
-        
+    
     email = data["email"]
     password = data["password"]
 
     user = users_collection.find_one({"email": email})
+    # if no user exists with that email, return an error
     if not user:
         return jsonify({"error": "Invalid email"}), 400
 
+    # unhashes the stored password and checks if it is the same as the plaintext password
     if not check_password_hash(user["password"], password)":
         return jsonify({"error": "Invalid password"}), 400
     
